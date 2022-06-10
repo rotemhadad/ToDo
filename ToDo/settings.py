cf9f2564d515619f70904b2a8ed46824d05653ed
leon-yarov <leon44ml@gmail.com>
2022-06-10 11:31:15 +0300

    fixed timezone

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TIME_ZONE = 'Europe/Berlin'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -105,8 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'Asia/kolkata'
 
 USE_I18N = True
 
